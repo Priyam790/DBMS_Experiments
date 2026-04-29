437a6b1d0ade72e304547229676690538cc1b217
Priyam Bhowmik <priyam.bhowmik.cs28@iilm.edu>
2026-04-30 05:11:32 +0530

    Update Experiment1.sql

--- Experiment1.sql
+++ Experiment1.sql
@@ -1,1 +1,16 @@
+ CREATE TABLE employee_master AS
+    -> SELECT * FROM employee;
 
+DELETE FROM employee_master
+    -> WHERE deptno = 10
+
+ UPDATE employee_master
+    -> SET sal = sal + (sal*0.10)
+    -> WHERE deptno = 20;
+
+ALTER TABLE employee_master
+    -> MODIFY sal DECIMAL (10,2);
+
+DROP TABLE employee_master;
+
+SELECT * FROM employee_master;
